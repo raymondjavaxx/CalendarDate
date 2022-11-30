80b7feb95ce5da98b61a5cbc98dc76e1dbddd221
Ramon Torres <raymondjavaxx@gmail.com>
2022-11-30 18:21:03 -0500

    Remove forced unwraps

--- Tests/CalendarDateTests/CodableTests.swift
+++ Tests/CalendarDateTests/CodableTests.swift
@@ -16,7 +16,7 @@ class CodableTests: XCTestCase {
             {"date": "2019-11-05T10:45:00", "timezone": "America/New_York"}
         """
 
-        let event = try decoder.decode(Event.self, from: json.data(using: .utf8)!)
+        let event = try decoder.decode(Event.self, from: try XCTUnwrap(json.data(using: .utf8)))
 
         XCTAssertEqual(event.date, CalendarDateTime(year: 2019, month: 11, day: 5, hour: 10, minute: 45))
     }
@@ -28,7 +28,7 @@ class CodableTests: XCTestCase {
         )
 
         let data = try encoder.encode(event)
-        let json = String(data: data, encoding: .utf8)!
+        let json = try XCTUnwrap(String(data: data, encoding: .utf8))
 
         XCTAssertTrue(json.contains("2019-11-05T10:45:00"))
     }
